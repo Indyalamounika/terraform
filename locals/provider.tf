21f9828a09060018d7a92ea4962e6861776b51bb
Indyalamounika <vedamouni01@gmail.com>
2025-10-28 12:40:47 +0530

    terraform

--- locals/provider.tf
+++ locals/provider.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "s3" {
     bucket = "remotestate-d"
-    key    = "locals-demo"
+    key    = "local-demo"
     region = "us-east-1"
     use_lockfile = true
     encrypt = true
